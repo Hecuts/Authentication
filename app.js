28084c301b8b41f8c1cf4f803c3f7dcbb99bfae4
Joe <yosefmesfin89@gmail.com>
2021-08-15 00:22:49 +0300

    signin with google/Google Strategy

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -19,7 +21,7 @@ app.use(express.urlencoded({
 app.use(session({
     secret: 'PutLongStringHere',
     resave: false,
-    saveUninitialized: true,    
+    saveUninitialized: true,
 }));
 // 3 - Initialize the session and use passport to manage the sessions
 app.use(passport.initialize());
@@ -35,24 +37,47 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-      //  required: true
+        //  required: true
     },
     password: {
         type: String,
-       // required: true
-    }
+        // required: true
+    },
+    googleId: String
 });
 
 // 4 - add a localmongoose plugin to the schema
 userSchema.plugin(passportLocalMongoose);
+// Add findOrCreate plugin to the schema 
+userSchema.plugin(findOrCreate);
 //model
 const User = new mongoose.model('User', userSchema);
 
 // 5- use passport-local-mongoose to create a strategy
 passport.use(User.createStrategy());
 // 6 - serialize and deserialize our user.
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done){
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: 'http://localhost:3000/auth/google/secrets',
+        userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({
+            googleId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -70,15 +95,29 @@ app.get('/secrets', function (req, res) {
         res.render('secrets');
     } else {
         res.redirect('/login');
-    }   
+    }
 });
 
 //log-out
-app.get('/logout', function(req, res){
+app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
 
+//google auth
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile']
+    }));
+
+//authentication callback
+app.get('/auth/google/secrets',
+    passport.authenticate('google', {
+        failureRedirect: '/login'
+    }),
+    function (req, res) {
+        res.redirect('/secrets');
+    });
 //Create a new user
 app.post('/register', function (req, res) {
     // 7 - use register from passport-local-mongoose
@@ -98,19 +137,19 @@ app.post('/register', function (req, res) {
 
 // Check the log-in 
 app.post('/login', function (req, res) {
-   const user = new User({
-       username: req.body.username,
-       password: req.body.password
-   });
-   req.logIn(user, function(err){
-       if(err){
-           console.log(err);
-       } else {
-           passport.authenticate('local')(req, res, function(){
-               res.redirect('/secrets');
-           });
-       }
-   });
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.logIn(user, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate('local')(req, res, function () {
+                res.redirect('/secrets');
+            });
+        }
+    });
 });
 
 app.listen(3000, console.log('Server started on port 3000'));
